bcfefc0d407ed7df477eb763f33c2b08756f9740
Krisztian Puska <pyx@pyx.space>
2017-07-03 14:09:13 +0200

    Prettier, remove unwanted files

--- components/head.js
+++ components/head.js
@@ -1,20 +1,28 @@
 import NextHead from "next/head"
 import { string } from "prop-types"
 
-const defaultDescription = ""
-const defaultOGURL = ""
-const defaultOGImage = ""
+const defaultDescription = "";
+const defaultOGURL = "";
+const defaultOGImage = "";
 
-const Head = (props) => (
+const Head = props =>
 	<NextHead>
 		<meta charset="UTF-8" />
-		<title>{props.title || ""}</title>
-		<meta name="description" content={props.description || defaultDescription} />
+		<title>
+			{props.title || ""}
+		</title>
+		<meta
+			name="description"
+			content={props.description || defaultDescription}
+		/>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="icon" href="/static/favicon.ico" />
 		<meta property="og:url" content={props.url || defaultOGURL} />
 		<meta property="og:title" content={props.title || ""} />
-		<meta property="og:description" content={props.description || defaultDescription} />
+		<meta
+			property="og:description"
+			content={props.description || defaultDescription}
+		/>
 		<meta name="twitter:site" content={props.url || defaultOGURL} />
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:image" content={props.ogImage || defaultOGImage} />
@@ -22,9 +30,12 @@ const Head = (props) => (
 		<meta property="og:image:width" content="1200" />
 		<meta property="og:image:height" content="630" />
 
-		<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet" />
+		<link
+			href="https://fonts.googleapis.com/css?family=Source+Code+Pro"
+			rel="stylesheet"
+		/>
 		<link href="https://unpkg.com/fractures@0.4.35/dist/fractures.prefixed.min.css" rel="stylesheet" />
-		<link href="/static/style.css" rel="stylesheet" />
+		<link href="/static/css/style.css" rel="stylesheet" />
 		{/* <script type="text/javascript">
 			(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -34,7 +45,6 @@ const Head = (props) => (
 			ga("send", "pageview");
 		</script> */}
 	</NextHead>
-)
 
 Head.propTypes = {
 	title: string,
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import Link from "next/link";
+import Head from "../components/head";
 
-export default () => (
+export default () =>
 	<div>
 		<Head title="Home" />
-		<Nav />
-
 		<main id="app">
-
 			{/* <!-- Hero --> */}
 			<div className="fr-container flex flex--rsp-l gutter-4 py-6">
-				<div><img src="/static/images/fractures.svg" alt="fractures" /></div>
+				<div>
+					<img src="/static/images/fractures.svg" alt="fractures" />
+				</div>
 				<div className="grow-2">
 					<h1 title="fractures">fr&middot;ctures</h1>
-					<b className="source-code-pro">Baseline atomic CSS toolkit.</b>
-					<p className="my-3">Fractures helps you bootstrap design-systems and prototypes by providing a set of non-blocking, atomic, utility classes.</p>
+					<b className="source-code-pro">
+						Baseline atomic CSS toolkit.
+					</b>
+					<p className="my-3">
+						Fractures helps you bootstrap design-systems and
+						prototypes by providing a set of non-blocking, atomic,
+						utility classes.
+					</p>
 
-					<a onclick="document.getElementById('getting-started').scrollIntoView(true);" className="btn btn-primary">Getting started</a>
-					<a href="https://github.com/fractures/fractures" className="btn btn-default ml-1">GitHub</a>
+					{/* <a onClick="document.getElementById('getting-started').scrollIntoView(true);" className="btn btn-primary">Getting started</a> */}
+					<a
+						href="https://github.com/fractures/fractures"
+						className="btn btn-default ml-1"
+					>
+						GitHub
+					</a>
 					<div className="mt-3 silver source-code-pro">
-						<small>version: 0.5.0<br />size: ~3kb gzipped</small>
+						<small>
+							version: 0.5.0<br />size: ~3kb gzipped
+						</small>
 					</div>
 				</div>
 			</div>
@@ -28,85 +39,90 @@ export default () => (
 			{/* <!-- Skeletons --> */}
 			<div className="skeleton py-5">
 				<div className="fr-container">
-
 					{/* <!-- Cases --> */}
 					<ul className="flex flex--around gutter-3">
 						<li
-
 							data-after="grid"
-							className="skeleton-item skeleton-item--grid"></li>
+							className="skeleton-item skeleton-item--grid"
+						/>
 						<li
-
 							data-after="flex"
-							className="skeleton-item skeleton-item--flex"></li>
+							className="skeleton-item skeleton-item--flex"
+						/>
 						<li
-
 							data-after="container"
-							className="skeleton-item skeleton-item--container"></li>
+							className="skeleton-item skeleton-item--container"
+						/>
 						<li
-
 							data-after="sticky"
-							className="skeleton-item skeleton-item--sticky"></li>
+							className="skeleton-item skeleton-item--sticky"
+						/>
 						<li
-
 							data-after="more"
-							className="skeleton-item skeleton-item--print"></li>
+							className="skeleton-item skeleton-item--print"
+						/>
 					</ul>
 
 					{/* <!-- Codes --> */}
-					<div
-						v-cloak
-						v-if="example"
-						className="relative mt-4 py-4">
-
+					<div className="relative mt-4 py-4">
 						{/* <!-- Grid --> */}
-						<pre
-							v-if="example === 'grid'"
-							className="pre-line">
-							<span className="color--orange">// Basic grid with horizontal and vertical gap</span>
-							&lt;div className="<span className="color--green">grid grid-cols-3 gap-2</span>"&gt;
-								&emsp;&lt;div&gt;&lt;/div&gt;
-								&emsp;&lt;div&gt;&lt;/div&gt;
-							&lt;/div&gt;
+						<pre className="pre-line">
+							<span className="color--orange">
+								// Basic grid with horizontal and vertical gap
+							</span>
+							&lt;div className="<span className="color--green">
+								grid grid-cols-3 gap-2
+							</span>"&gt; &emsp;&lt;div&gt;&lt;/div&gt;
+							&emsp;&lt;div&gt;&lt;/div&gt; &lt;/div&gt;
 						</pre>
 
 						{/* <!-- Flex --> */}
-						<pre
-							v-if="example === 'flex'"
-							className="pre-line">
-							<span className="color--orange">// Centered flex content</span>
-							&lt;div className="<span className="color--green">flex flex--center</span>"&gt;
-								&emsp;&lt;div&gt;&lt;/div&gt;
+						<pre className="pre-line">
+							<span className="color--orange">
+								// Centered flex content
+							</span>
+							&lt;div className="<span className="color--green">
+								flex flex--center
+							</span>"&gt; &emsp;&lt;div&gt;&lt;/div&gt;
 							&lt;/div&gt;
 						</pre>
 
 						{/* <!-- Container --> */}
-						<pre
-							v-if="example === 'container'"
-							className="pre-line">
-							<span className="color--orange">// Responsive containers based on material design breakpoints</span>
-							&lt;div className="<span className="color--green">fr-container-small</span>"&gt;&lt;/div&gt;
-							&lt;div className="<span className="color--green">fr-container</span>"&gt;&lt;/div&gt;
-							&lt;div className="<span className="color--green">fr-container-large</span>"&gt;&lt;/div&gt;
+						<pre className="pre-line">
+							<span className="color--orange">
+								// Responsive containers based on material
+								design breakpoints
+							</span>
+							&lt;div className="<span className="color--green">
+								fr-container-small
+							</span>"&gt;&lt;/div&gt; &lt;div className="<span className="color--green">fr-container</span>"&gt;&lt;/div&gt;
+							&lt;div className="<span className="color--green">
+								fr-container-large
+							</span>"&gt;&lt;/div&gt;
 						</pre>
 
 						{/* <!-- Sticky --> */}
-						<pre
-							v-if="example === 'sticky'"
-							className="pre-line">
-							<span className="color--orange">// Sticky footer with minimum element height</span>
-							&lt;div className="<span className="color--green">flex flex--column minvh-100</span>"&gt;
-								&emsp;&lt;div className="<span className="color--green">grow-1</span>"&gt;&lt;/div&gt;
-								&emsp;&lt;footer&gt;&lt;/footer&gt;
-							&lt;/div&gt;
+						<pre className="pre-line">
+							<span className="color--orange">
+								// Sticky footer with minimum element height
+							</span>
+							&lt;div className="<span className="color--green">
+								flex flex--column minvh-100
+							</span>"&gt; &emsp;&lt;div className="<span className="color--green">grow-1</span>"&gt;&lt;/div&gt;
+							&emsp;&lt;footer&gt;&lt;/footer&gt; &lt;/div&gt;
 						</pre>
 
 						{/* <!-- Print --> */}
-						<pre
-							v-if="example === 'print'"
-							className="pre-line">
-							<span className="color--orange">// Working examples</span>
-							<a href="http://fractures.github.io/fractures/" className="color--orange">// fractures.github.io/fractures</a>
+						<pre className="pre-line">
+							<span className="color--orange">
+								// Working examples
+							</span>
+							<a
+								href="http://fractures.github.io/fractures/"
+								className="color--orange"
+							>
+								// fractures.github.io/fractures
+							</a>
 						</pre>
 					</div>
 				</div>
@@ -114,20 +130,38 @@ export default () => (
 
 			{/* <!-- Content --> */}
 			<div className="fr-container py-5" id="getting-started">
-
 				{/* <!-- Getting started --> */}
 				<h2 className="regular mb-1">Getting started</h2>
 				<ul className="list-disc mb-4 ml-3">
 					<li>npm install fractures</li>
-					<li>download vanilla &rarr; <a href="https://unpkg.com/fractures/dist/fractures.min.css">fractures.min.css</a></li>
-					<li>download prefixed for latest 2 browser version &rarr; <a href="https://unpkg.com/fractures/dist/fractures.prefixed.min.css">fractures.prefixed.min.css</a></li>
-					<li>use <a href="https://unpkg.com/fractures/dist/fractures.prefixed.min.css">unpkg</a> cdn</li>
+					<li>
+						download vanilla &rarr;{" "}
+						<a href="https://unpkg.com/fractures/dist/fractures.min.css">
+							fractures.min.css
+						</a>
+					</li>
+					<li>
+						download prefixed for latest 2 browser version &rarr;{" "}
+						<a href="https://unpkg.com/fractures/dist/fractures.prefixed.min.css">
+							fractures.prefixed.min.css
+						</a>
+					</li>
+					<li>
+						use{" "}
+						<a href="https://unpkg.com/fractures/dist/fractures.prefixed.min.css">
+							unpkg
+						</a>{" "}
+						cdn
+					</li>
 				</ul>
 
 				{/* <!-- Goals --> */}
 				<h2 className="regular mb-1">Goals</h2>
 				<ul className="list-disc mb-4 ml-3">
-					<li>You can design with &mdash; not around it. Leaves UI untouched.</li>
+					<li>
+						You can design with &mdash; not around it. Leaves UI
+						untouched.
+					</li>
 					<li>Atomic, non blocking.</li>
 					<li>Managable cognitive load, with a small footprint.</li>
 					<li>Fast in every way.</li>
@@ -136,22 +170,18 @@ export default () => (
 				{/* <!-- Docs --> */}
 				<h2 className="regular mb-1">List of classes</h2>
 				<ul className="docs list-disc ml-3">
-
 					{/* <!-- Blocks --> */}
-					<li v-for="(block, name) in blocks" className="mb-2">
-
+					<li className="mb-2">
 						{/* <!-- Sets --> */}
-						<p v-html="name"></p>
-						<div v-for="(array, index) in block">
+						<p />
+						<div>
 							<div className="flex flex--wrap gutter-2">
-								<div v-html="index + ' &rarr;'" className="docs-class silver"></div>
-								<div v-for="elem in array" v-html="elem" className="class"></div>
+								<div className="docs-class silver" />
+								<div className="class" />
 							</div>
 						</div>
 					</li>
 				</ul>
 			</div>
 		</main>
-
-	</div>
-)
+	</div>;
