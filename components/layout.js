c1703a3a820ea0788cb34a0777a698c434e8447c
Krisztian Puska <pyx@pyx.space>
2017-07-07 09:03:09 +0200

    Fix scope typo

--- components/layout.js
+++ components/layout.js
@@ -4,12 +4,11 @@ import Nav from "./nav.js"
 import { Component } from "react"
 import { initGA, logPageView } from "../utils/analytics"
 
+const description = "Baseline atomic CSS toolkit."
+const title = "fractures"
+const url = "https://fractures.space"
+
 export default class Layout extends Component {
-
-	const description = "Baseline atomic CSS toolkit."
-	const title = "fractures"
-	const url = "https://fractures.space"
-
 	// Init GA on mount
 	componentDidMount() {
 		if (!window.GA_INITIALIZED) {
@@ -29,16 +28,22 @@ export default class Layout extends Component {
 					</title>
 					<meta charSet="utf-8" />
 					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-					<meta name="description" content={ description } />
-					<meta name="og-url" property="og:url" content={ url } />
+					<meta name="description" content={description} />
+					<meta name="og-url" property="og:url" content={url} />
 					<meta name="og-type" property="og:type" content="website" />
-					<meta name="og-title" property="og:title" content={ title } />
-					<meta name="og-description" property="og:description" content={ description } />
-					<meta name="og-image" property="og:image"
-						content="https://fractures.space/static/images/share.png" />
+					<meta name="og-title" property="og:title" content={title} />
+					<meta name="og-description" property="og:description" content={description} />
+					<meta
+						name="og-image"
+						property="og:image"
+						content="https://fractures.space/static/images/share.png"
+					/>
 					<meta name="og-image-width" property="og:image:width" content="200" />
 					<meta name="og-image-height" property="og:image:height" content="200" />
-					<meta name="google-site-verification" content="zOr1hPUy598X8UrDiYZLK20AkpuhWw0PYKcdOJ-NzPU" />
+					<meta
+						name="google-site-verification"
+						content="zOr1hPUy598X8UrDiYZLK20AkpuhWw0PYKcdOJ-NzPU"
+					/>
 					<link rel="icon" href="/static/favicon.ico" />
 					<link
 						href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700"
