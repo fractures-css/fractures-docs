7ebf9b6b1e7544604742e025803d8d283b8e7f3c
Krisztian Puska <pyx@pyx.space>
2017-09-01 20:21:10 +0200

    Update to latest fractures

--- utils/meta.js
+++ utils/meta.js
@@ -1,8 +1,8 @@
-const version = "1.0.3"
+const version = "1.0.4"
 const fracturesMeta = {
 	cdn: `https://unpkg.com/fractures@${ version }/dist/fractures.prefixed.min.css`,
 	description: "Baseline atomic CSS toolkit.",
-	size: "2762",
+	size: "2644",
 	title: "fractures",
 	url: "https://fractures.space",
 	version: version
