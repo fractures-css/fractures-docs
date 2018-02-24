a07bf0d1878875f894de2d1f8ff0d1a97283c3e0
Krisztian Puska <pyx@pyx.space>
2018-02-24 15:55:47 +0100

    Page components

---
+++ pages/ui/components/UiHero.js
@@ -0,0 +1,68 @@
+import { Fragment } from "react"
+
+const UiHero = () => (
+	<Fragment>
+		<header className="pb-4">
+			<h1>UI</h1>
+			<p className="mb-2">
+				Remember when I said framework agnostic?<br />Fractures UI is a small-but-nice, stateless component
+				library for <b>React</b>, built on top of{" "}
+				<Link href="/">
+					<a>fractures</a>
+				</Link>.
+			</p>
+			<p>
+				This is really just an experimental implementation; not included in fractures.css.<br />
+				Open an{" "}
+				<a href="https://github.com/fractures/fractures-ui" target="_new">
+					issue
+				</a>, and tell me what you think.
+			</p>
+		</header>
+		<div className="pb-4">
+			<h3>Let me try</h3>
+			<ul className="list-disc ml-4 mt-1">
+				<li>
+					<code>npm i fractures-ui</code>.
+				</li>
+				<li>
+					<code>
+						import {`{`} Button {`}`} from "fractures-ui"
+					</code>.
+				</li>
+				<li>
+					See code{" "}
+					<a href="https://github.com/fractures/fractures-docs/blob/master/pages/ui.js#L69">examples</a>.
+				</li>
+				<li>
+					Define the following css variables for :root.
+					<pre>
+						<code>--fr-ground: #ffffff;</code>
+						<br />
+						<code>--fr-100: #e8f2fd;</code>
+						<br />
+						<code>--fr-300: #76a7e7;</code>
+						<br />
+						<code>--fr-500: #3482da;</code>
+						<br />
+						<code>--fr-700: #2463a5;</code>
+						<br />
+						<code>--fr-900: #102a45;</code>
+						<br />
+						<code>--fr-focus: rgba(52, 130, 218, 0.4);</code>
+						<br />
+					</pre>
+				</li>
+			</ul>
+		</div>
+		<div className="pb-4">
+			<h3>Goals</h3>
+			<ul className="list-disc ml-4 mt-1">
+				<li>Minimum viable design</li>
+				<li>Functional prototypes</li>
+			</ul>
+		</div>
+	</Fragment>
+)
+
+export default UiHero
--- pages/ui/ui.js
+++ pages/ui/ui.js
@@ -1,6 +1,5 @@
 import { Button, ButtonGroup, Checkbox, Color, Container, Fractures, Radio, Range, Progress, Input } from "fractures-ui"
 import { Component } from "react"
-import chroma from "chroma-js"
 import Layout from "../../components/Layout"
 import Link from "next/link"
 
@@ -63,68 +62,7 @@ class fUI extends Component {
 					))}
 				</aside>
 				<Container className="py-4">
-					<header className="pb-4">
-						<h1>UI</h1>
-						<p className="mb-2">
-							Remember when I said framework agnostic?<br />Fractures UI is a small-but-nice, stateless
-							component library for <b>React</b>, built on top of{" "}
-							<Link href="/">
-								<a>fractures</a>
-							</Link>.
-						</p>
-						<p>
-							This is really just an experimental implementation; not included in fractures.css.<br />
-							Open an{" "}
-							<a href="https://github.com/fractures/fractures-ui" target="_new">
-								issue
-							</a>, and tell me what you think.
-						</p>
-					</header>
-					<div className="pb-4">
-						<h3>Let me try</h3>
-						<ul className="list-disc ml-4 mt-1">
-							<li>
-								<code>npm i fractures-ui</code>.
-							</li>
-							<li>
-								<code>
-									import {`{`} Button {`}`} from "fractures-ui"
-								</code>.
-							</li>
-							<li>
-								See code{" "}
-								<a href="https://github.com/fractures/fractures-docs/blob/master/pages/ui.js#L69">
-									examples
-								</a>.
-							</li>
-							<li>
-								Define the following css variables for :root.
-								<pre>
-									<code>--fr-ground: #ffffff;</code>
-									<br />
-									<code>--fr-100: #e8f2fd;</code>
-									<br />
-									<code>--fr-300: #76a7e7;</code>
-									<br />
-									<code>--fr-500: #3482da;</code>
-									<br />
-									<code>--fr-700: #2463a5;</code>
-									<br />
-									<code>--fr-900: #102a45;</code>
-									<br />
-									<code>--fr-focus: rgba(52, 130, 218, 0.4);</code>
-									<br />
-								</pre>
-							</li>
-						</ul>
-					</div>
-					<div className="pb-4">
-						<h3>Goals</h3>
-						<ul className="list-disc ml-4 mt-1">
-							<li>Minimum viable design</li>
-							<li>Functional prototypes</li>
-						</ul>
-					</div>
+					<UiHero />
 					<div className="my-4">
 						<h4 className="my-2">Buttons</h4>
 						<div className="flex flex-gap-1 my-1">
