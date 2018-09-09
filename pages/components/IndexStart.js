6427daab70948cfb8d7f5ae4095e2d573673818b
Krisztian Puska <pyx@pyx.space>
2018-09-09 15:09:04 +0200

    Add hacks for colors

--- pages/components/IndexStart.js
+++ pages/components/IndexStart.js
@@ -9,26 +9,31 @@ const IndexStart = () => (
 			<h2>Getting started</h2>
 			<ul className="flex flex-column flex-gap-4 my-4">
 				<li>
+					<b>npm i fractures</b>
+					<ul className="list-disc ml-4">
+						<li>
+							Then you can 🦄 <code>import "fractures/dist/fractures.css"</code>
+						</li>
+						<li>For production use, I recommend running the fractures.css file through your build process.</li>
+						<li>
+							You can find usage examples under <a href="https://github.com/fractures/fractures-examples">fractures-examples</a> for{" "}
+							<b>React</b> and <b>Next.js</b>.
+						</li>
+					</ul>
+				</li>
+				<li>
 					<b>Use unpkg cdn</b>
 					<ul className="list-disc ml-4">
 						<li>
-							<div className="float-left ellipsis">&lt;link href="{meta.cdn}" rel="stylesheet" /&gt;</div>
+							<code className="float-left ellipsis">
+								&lt;link href="
+								{meta.cdn}" rel="stylesheet" /&gt;
+							</code>
 						</li>
 						<li>This is not great for production, but great for prototypes or lazy people.</li>
 					</ul>
 				</li>
-				<li>
-					<b>npm i fractures</b>
-					<ul className="list-disc ml-4">
-						<li>
-							Then you can 🦄 <em>import "fractures/dist/fractures.css"</em>
-						</li>
-						<li>For production use, I recommend running the fractures.css file through your build process.</li>
-						<li>
-							Here is an example repo <a href="https://github.com/fractures/fractures-examples">with React</a>.
-						</li>
-					</ul>
-				</li>
+
 				<li>
 					<b>Download with your hands</b>
 					<ul className="list-disc ml-4">
