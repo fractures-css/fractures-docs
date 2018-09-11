34f3140fe212d0ab916aa0fe9b1573a3c720f82a
Krisztian Puska <pyx@pyx.space>
2018-09-11 17:59:14 +0200

    Refactor with a component repeater

---
+++ components/Repeater.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const Repeater = props => {
+	const Component = props.component
+
+	return (
+		<Fragment>
+			{props.scale.map((item, key) => (
+				<Component key={ key } { ...props } label={ props.label.replace("$", item) } />
+			))}
+		</Fragment>
+	)
+}
+
+export default Repeater
--- pages/docs.js
+++ pages/docs.js
@@ -3,8 +3,24 @@ import DocsElement from "./components/DocsElement"
 import DocsSection from "./components/DocsSection"
 import Layout from "../components/Layout"
 import Link from "next/link"
+import Repeater from "../components/Repeater"
 
-const scale = [1, 2, 3, 4, 5, 6]
+// Basic scales
+const baseScale = [1, 2, 3, 4, 5, 6]
+const baseScaleZero = [0, ...baseScale]
+const shallowScale = baseScaleZero.slice(0, 4)
+
+// Specific scale for heights and width slices
+const scaleSpace = [10, 20, 25, 30, 33, 40, 50, 60, 66, 70, 75, 80, 90, 100]
+
+// Z space scale
+const zScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Decimal scale
+const decimalScale = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
+// Column scales
+const columnScale = [2, 3, 4, 5, 6, 7, 8]
 
 const Docs = () => (
 	<Layout title="Documentation — fractures">
@@ -59,10 +75,7 @@ const Docs = () => (
 				description="Border radius is important."
 				notes={ [{ name: ".radius-max", note: "is rounding border with a large pixel value. Creates tube." }] }
 			>
-				<DocsElement label="radius-0" showClass className="bg-white" disableBackground />
-				<DocsElement label="radius-1" showClass />
-				<DocsElement label="radius-2" showClass />
-				<DocsElement label="radius-3" showClass />
+				<Repeater component={ DocsElement } label="radius-$" scale={ shallowScale } showClass />
 				<DocsElement label="radius-max" showClass className="bg-color-dark white" disableBackground isMarked />
 				<DocsElement label="radius-100" showClass className="bg-color-dark white" disableBackground />
 			</DocsSection>
@@ -154,15 +167,11 @@ const Docs = () => (
 				<DocsElement label="flex-baseline" />
 				<DocsElement label="flex-end" />
 				<DocsElement label="flex-start" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `flex-gap-${ step }` } />
-				))}
+				<Repeater component={ DocsElement } label="flex-gap-$" scale={ baseScale } />
 				<DocsElement label="flex-center" isMarked />
 				<DocsElement label="flex-xcenter" isMarked />
 				<DocsElement label="flex-ycenter" isMarked />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `grow-${ step }` } />
-				))}
+				<Repeater component={ DocsElement } label="grow-$" scale={ baseScale } />
 				<DocsElement label="self-center" />
 				<DocsElement label="self-start" />
 				<DocsElement label="self-end" />
@@ -176,31 +185,13 @@ const Docs = () => (
 					{ name: ".grid-(rows|cols)-x", note: "has a .dense class modifier" }
 				] }
 			>
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `grid-gap-${ step }` } />
-				))}
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `grid-gap-x-${ step }` } />
-				))}
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `grid-gap-y-${ step }` } />
-				))}
+				<Repeater component={ DocsElement } label="grid-gap-$" scale={ baseScale } />
+				<Repeater component={ DocsElement } label="grid-gap-x-$" scale={ baseScale } />
+				<Repeater component={ DocsElement } label="grid-gap-y-$" scale={ baseScale } />
 				<DocsElement label="grid-cols" />
-				<DocsElement label="grid-cols-2" />
-				<DocsElement label="grid-cols-3" />
-				<DocsElement label="grid-cols-4" />
-				<DocsElement label="grid-cols-5" />
-				<DocsElement label="grid-cols-6" />
-				<DocsElement label="grid-cols-7" />
-				<DocsElement label="grid-cols-6" />
+				<Repeater component={ DocsElement } label="grid-cols-$" scale={ columnScale } />
 				<DocsElement label="grid-rows" />
-				<DocsElement label="grid-rows-2" />
-				<DocsElement label="grid-rows-3" />
-				<DocsElement label="grid-rows-4" />
-				<DocsElement label="grid-rows-5" />
-				<DocsElement label="grid-rows-6" />
-				<DocsElement label="grid-rows-7" />
-				<DocsElement label="grid-rows-8" />
+				<Repeater component={ DocsElement } label="grid-rows-$" scale={ columnScale } />
 			</DocsSection>
 			<DocsSection
 				title="Height"
@@ -212,54 +203,9 @@ const Docs = () => (
 					{ name: ".minh-viewport", note: "is mininum height set to current viewport height." }
 				] }
 			>
-				<DocsElement label="h-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `h-${ step }` } />
-				))}
-				<DocsElement label="h-10" />
-				<DocsElement label="h-20" />
-				<DocsElement label="h-25" />
-				<DocsElement label="h-30" />
-				<DocsElement label="h-33" />
-				<DocsElement label="h-40" />
-				<DocsElement label="h-50" />
-				<DocsElement label="h-60" />
-				<DocsElement label="h-66" />
-				<DocsElement label="h-70" />
-				<DocsElement label="h-75" />
-				<DocsElement label="h-80" />
-				<DocsElement label="h-90" />
-				<DocsElement label="h-100" />
-				<DocsElement label="maxh-10" />
-				<DocsElement label="maxh-20" />
-				<DocsElement label="maxh-25" />
-				<DocsElement label="maxh-30" />
-				<DocsElement label="maxh-33" />
-				<DocsElement label="maxh-40" />
-				<DocsElement label="maxh-50" />
-				<DocsElement label="maxh-60" />
-				<DocsElement label="maxh-66" />
-				<DocsElement label="maxh-70" />
-				<DocsElement label="maxh-75" />
-				<DocsElement label="maxh-80" />
-				<DocsElement label="maxh-90" />
-				<DocsElement label="maxh-100" />
-				<DocsElement label="maxh-viewport" />
-				<DocsElement label="minh-10" />
-				<DocsElement label="minh-20" />
-				<DocsElement label="minh-25" />
-				<DocsElement label="minh-30" />
-				<DocsElement label="minh-33" />
-				<DocsElement label="minh-40" />
-				<DocsElement label="minh-50" />
-				<DocsElement label="minh-60" />
-				<DocsElement label="minh-66" />
-				<DocsElement label="minh-70" />
-				<DocsElement label="minh-75" />
-				<DocsElement label="minh-80" />
-				<DocsElement label="minh-90" />
-				<DocsElement label="minh-100" />
-				<DocsElement label="minh-viewport" />
+				<Repeater component={ DocsElement } label="h-$" scale={ [...baseScaleZero, ...scaleSpace] } />
+				<Repeater component={ DocsElement } label="maxh-$" scale={ [...scaleSpace, "viewport"] } />
+				<Repeater component={ DocsElement } label="minh-$" scale={ [...scaleSpace, "viewport"] } />
 			</DocsSection>
 			<DocsSection
 				title="List"
@@ -284,48 +230,16 @@ const Docs = () => (
 				description="Common margin options."
 				notes={ [{ name: ".mx-x", note: "is margin on X axis." }, { name: ".mx-y", note: "is margin on Y axis." }] }
 			>
-				<DocsElement label="m-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `m-${ step }` } />
-				))}
-				<DocsElement label="mb-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `mb-${ step }` } />
-				))}
-				<DocsElement label="ml-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `ml-${ step }` } />
-				))}
-				<DocsElement label="mt-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `mt-${ step }` } />
-				))}
-				<DocsElement label="mr-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `mr-${ step }` } />
-				))}
-				<DocsElement label="my-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `my-${ step }` } />
-				))}
-				<DocsElement label="mx-0" />
-				{scale.map((step, key) => (
-					<DocsElement key={ key } label={ `mx-${ step }` } />
-				))}
-				<DocsElement label="mx-auto" />
+				<Repeater component={ DocsElement } label="m-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="mb-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="mr-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="ml-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="mt-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="my-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="mx-$" scale={ [...baseScaleZero, "auto"] } />
 			</DocsSection>
 			<DocsSection title="Opacity" description="Opacity by decimal steps.">
-				<DocsElement label="opacity-100" showClass />
-				<DocsElement label="opacity-90" showClass />
-				<DocsElement label="opacity-80" showClass />
-				<DocsElement label="opacity-70" showClass />
-				<DocsElement label="opacity-60" showClass />
-				<DocsElement label="opacity-50" showClass />
-				<DocsElement label="opacity-40" showClass />
-				<DocsElement label="opacity-30" showClass />
-				<DocsElement label="opacity-20" showClass />
-				<DocsElement label="opacity-10" showClass />
-				<DocsElement label="opacity-0" showClass />
+				<Repeater component={ DocsElement } label="opacity-$" scale={ decimalScale } showClass />
 			</DocsSection>
 			<DocsSection title="Overflow" description="Content overflows.">
 				<DocsElement label="opacity-visible" showClass />
@@ -337,55 +251,13 @@ const Docs = () => (
 				description="Paddings based on overwriteable box sizes."
 				notes={ [{ name: ".py-x", note: "is padding on X axis." }, { name: ".py-y", note: "is padding on Y axis." }] }
 			>
-				<DocsElement label="p-0" />
-				<DocsElement label="p-1" />
-				<DocsElement label="p-2" />
-				<DocsElement label="p-3" />
-				<DocsElement label="p-4" />
-				<DocsElement label="p-5" />
-				<DocsElement label="p-6" />
-				<DocsElement label="pb-0" />
-				<DocsElement label="pb-1" />
-				<DocsElement label="pb-2" />
-				<DocsElement label="pb-3" />
-				<DocsElement label="pb-4" />
-				<DocsElement label="pb-5" />
-				<DocsElement label="pb-6" />
-				<DocsElement label="pl-0" />
-				<DocsElement label="pl-1" />
-				<DocsElement label="pl-2" />
-				<DocsElement label="pl-3" />
-				<DocsElement label="pl-4" />
-				<DocsElement label="pl-5" />
-				<DocsElement label="pl-6" />
-				<DocsElement label="pt-0" />
-				<DocsElement label="pt-1" />
-				<DocsElement label="pt-2" />
-				<DocsElement label="pt-3" />
-				<DocsElement label="pt-4" />
-				<DocsElement label="pt-5" />
-				<DocsElement label="pt-6" />
-				<DocsElement label="pr-0" />
-				<DocsElement label="pr-1" />
-				<DocsElement label="pr-2" />
-				<DocsElement label="pr-3" />
-				<DocsElement label="pr-4" />
-				<DocsElement label="pr-5" />
-				<DocsElement label="pr-6" />
-				<DocsElement label="py-0" />
-				<DocsElement label="py-1" />
-				<DocsElement label="py-2" />
-				<DocsElement label="py-3" />
-				<DocsElement label="py-4" />
-				<DocsElement label="py-5" />
-				<DocsElement label="py-6" />
-				<DocsElement label="px-0" />
-				<DocsElement label="px-1" />
-				<DocsElement label="px-2" />
-				<DocsElement label="px-3" />
-				<DocsElement label="px-4" />
-				<DocsElement label="px-5" />
-				<DocsElement label="px-6" />
+				<Repeater component={ DocsElement } label="p-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="pb-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="pr-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="pl-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="pt-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="py-$" scale={ baseScaleZero } />
+				<Repeater component={ DocsElement } label="px-$" scale={ baseScaleZero } />
 			</DocsSection>
 			<DocsSection
 				title="Position"
@@ -396,32 +268,11 @@ const Docs = () => (
 				<DocsElement label="fixed" />
 				<DocsElement label="relative" />
 				<DocsElement label="sticky" />
-				<DocsElement label="bottom-0" />
-				<DocsElement label="bottom-1" />
-				<DocsElement label="bottom-2" />
-				<DocsElement label="bottom-3" />
-				<DocsElement label="left-0" />
-				<DocsElement label="left-1" />
-				<DocsElement label="left-2" />
-				<DocsElement label="left-3" />
-				<DocsElement label="right-0" />
-				<DocsElement label="right-1" />
-				<DocsElement label="right-2" />
-				<DocsElement label="right-3" />
-				<DocsElement label="top-0" />
-				<DocsElement label="top-1" />
-				<DocsElement label="top-2" />
-				<DocsElement label="top-3" />
-				<DocsElement label="z-0" />
-				<DocsElement label="z-1" />
-				<DocsElement label="z-2" />
-				<DocsElement label="z-3" />
-				<DocsElement label="z-4" />
-				<DocsElement label="z-5" />
-				<DocsElement label="z-6" />
-				<DocsElement label="z-7" />
-				<DocsElement label="z-9" />
-				<DocsElement label="z-10" />
+				<Repeater component={ DocsElement } label="bottom-$" scale={ shallowScale } />
+				<Repeater component={ DocsElement } label="left-$" scale={ shallowScale } />
+				<Repeater component={ DocsElement } label="right-$" scale={ shallowScale } />
+				<Repeater component={ DocsElement } label="top-$" scale={ shallowScale } />
+				<Repeater component={ DocsElement } label="z-$" scale={ zScale } />
 			</DocsSection>
 			<DocsSection
 				title="Typography"
@@ -471,57 +322,9 @@ const Docs = () => (
 					{ name: ".minw-viewport", note: "is minimum width set to current viewport width." }
 				] }
 			>
-				<DocsElement label="w-0" />
-				<DocsElement label="w-1" />
-				<DocsElement label="w-2" />
-				<DocsElement label="w-3" />
-				<DocsElement label="w-4" />
-				<DocsElement label="w-5" />
-				<DocsElement label="w-6" />
-				<DocsElement label="w-10" />
-				<DocsElement label="w-20" />
-				<DocsElement label="w-25" />
-				<DocsElement label="w-30" />
-				<DocsElement label="w-33" />
-				<DocsElement label="w-40" />
-				<DocsElement label="w-50" />
-				<DocsElement label="w-60" />
-				<DocsElement label="w-66" />
-				<DocsElement label="w-70" />
-				<DocsElement label="w-75" />
-				<DocsElement label="w-80" />
-				<DocsElement label="w-90" />
-				<DocsElement label="w-100" />
-				<DocsElement label="maxw-10" />
-				<DocsElement label="maxw-20" />
-				<DocsElement label="maxw-25" />
-				<DocsElement label="maxw-30" />
-				<DocsElement label="maxw-33" />
-				<DocsElement label="maxw-40" />
-				<DocsElement label="maxw-50" />
-				<DocsElement label="maxw-60" />
-				<DocsElement label="maxw-66" />
-				<DocsElement label="maxw-70" />
-				<DocsElement label="maxw-75" />
-				<DocsElement label="maxw-80" />
-				<DocsElement label="maxw-90" />
-				<DocsElement label="maxw-100" />
-				<DocsElement label="maxw-viewport" />
-				<DocsElement label="minw-10" />
-				<DocsElement label="minw-20" />
-				<DocsElement label="minw-25" />
-				<DocsElement label="minw-30" />
-				<DocsElement label="minw-33" />
-				<DocsElement label="minw-40" />
-				<DocsElement label="minw-50" />
-				<DocsElement label="minw-60" />
-				<DocsElement label="minw-66" />
-				<DocsElement label="minw-70" />
-				<DocsElement label="minw-75" />
-				<DocsElement label="minw-80" />
-				<DocsElement label="minw-90" />
-				<DocsElement label="minw-100" />
-				<DocsElement label="minw-viewport" />
+				<Repeater component={ DocsElement } label="w-$" scale={ [...baseScaleZero, ...scaleSpace] } />
+				<Repeater component={ DocsElement } label="maxw-$" scale={ [...scaleSpace, "viewport"] } />
+				<Repeater component={ DocsElement } label="minw-$" scale={ [...scaleSpace, "viewport"] } />
 			</DocsSection>
 		</Container>
 	</Layout>
