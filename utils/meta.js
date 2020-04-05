426aa4f4ae59250ea975fbaff625624aae88263e
Krisztian Puska <pyx@pyx.space>
2020-04-04 17:50:22 -0700

    Update packages

--- utils/meta.js
+++ utils/meta.js
@@ -1,9 +1,9 @@
-const version = '1.3.3'
+const version = '1.3.4'
 const fracturesMeta = {
 	cdn: `https://unpkg.com/fractures@${ version }/dist/fractures.prefixed.min.css`,
 	description: 'Baseline atomic CSS toolkit.',
 	size: '3290',
-	stars: 353,
+	stars: 359,
 	title: 'fractures',
 	url: 'https://fractures.space',
 	version: version
